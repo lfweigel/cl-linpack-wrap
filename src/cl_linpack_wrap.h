1a9f200e11990121f2e9d329e0d933e59fa1f182
jackallen0714 <jackallen0714@gmail.com>
2013-11-01 21:50:37 +0000

    * Structure is ready, need to fill the content now

---
+++ src/cl_linpack_wrap.h
@@ -0,0 +1,16 @@
+#ifndef CL_LINPACK_WRAP
+#define CL_LINPACK_WRAP
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Defines */
+
+/* Functions */
+
+#ifdef __cplusplus
+}      /* extern "C" { */
+#endif
+
+#endif /* CL_LINPACK_WRAP */
